0565f9b25052417fd4286a62bec6221e52296c90
klinson <klinson@163.com>
2019-10-26 17:52:43 +0800

    兼容地区表欠缺下区不存在情况

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -26,7 +26,11 @@ class Address extends Model
 
     public function getAllCityNameAttribute()
     {
-        return $this->district->full_name;
+        if ($this->district) {
+            return $this->district->full_name;
+        } else {
+            return $this->city->full_name;
+        }
     }
 
     public function toSnapshot()
@@ -40,5 +44,10 @@ class Address extends Model
     public function district()
     {
         return $this->belongsTo(Area::class, 'district_code', 'code');
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(Area::class, 'city_code', 'code');
     }
 }
