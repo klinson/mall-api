8679c038e419da56852d8646ffb344c1fb325464
klinson <klinson@163.com>
2019-10-27 15:05:47 +0800

    后台首页

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -16,199 +16,196 @@ class HomeController extends Controller
 {
     protected $pageHeader = 'Dashboard';
 
-    public function index()
+    public function index(Content $content)
     {
-        return Admin::content(function (Content $content) {
-            $this->_setPageDefault($content);
+        $this->_setPageDefault($content);
+        $content->row(view('admin.dashboard.title'));
+        $list = [
+            [
+                'title' => '今日任务',
+                'width' => 4,
+                'data' => [
+                    [
+                        'title' => '待发货',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders?status[]=2',
+                        'info' => Order::where('status', 2)->count(),
+                    ],
+                    [
+                        'title' => '售后待审核',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/refundOrders?status[]=1',
+                        'info' => RefundOrder::where('status', 1)->count(),
+                    ],
+                    [
+                        'title' => '售后待确认',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/refundOrders?status[]=3',
+                        'info' => RefundOrder::where('status', 3)->count(),
+                    ],
+                ]
+            ],
+            [
+                'title' => '商场订单下单量',
+                'width' => 4,
+                'data' => [
+                    [
+                        'title' => '今日',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inToday('created_at')->count(),
+                    ],
+                    [
+                        'title' => '本周',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inWeek('created_at')->count(),
+                    ],
+                    [
+                        'title' => '本月',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inMonth('created_at')->count(),
+                    ],
+                    [
+                        'title' => '昨日',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inYesterday('created_at')->count(),
+                    ],
+                    [
+                        'title' => '上周',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inLastWeek('created_at')->count(),
+                    ],
+                    [
+                        'title' => '上月',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inLastMonth('created_at')->count(),
+                    ],
+                ]
+            ],
+            [
+                'title' => '商场订单支付量',
+                'width' => 4,
+                'data' => [
+                    [
+                        'title' => '今日',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inToday('payed_at')->count(),
+                    ],
+                    [
+                        'title' => '本周',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inWeek('payed_at')->count(),
+                    ],
+                    [
+                        'title' => '本月',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inMonth('payed_at')->count(),
+                    ],
+                    [
+                        'title' => '昨日',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inYesterday('payed_at')->count(),
+                    ],
+                    [
+                        'title' => '上周',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inLastWeek('payed_at')->count(),
+                    ],
+                    [
+                        'title' => '上月',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::inLastMonth('payed_at')->count(),
+                    ],
+                ]
+            ],
+            [
+                'title' => '商场订单完成量',
+                'width' => 4,
+                'data' => [
+                    [
+                        'title' => '今日',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::where('status', 4)->inToday('confirmed_at')->count(),
+                    ],
+                    [
+                        'title' => '本周',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::where('status', 4)->inWeek('confirmed_at')->count(),
+                    ],
+                    [
+                        'title' => '本月',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::where('status', 4)->inMonth('confirmed_at')->count(),
+                    ],
+                    [
+                        'title' => '昨日',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::where('status', 4)->inYesterday('confirmed_at')->count(),
+                    ],
+                    [
+                        'title' => '上周',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::where('status', 4)->inLastWeek('confirmed_at')->count(),
+                    ],
+                    [
+                        'title' => '上月',
+                        'icon' => 'cart-plus',
+                        'color' => 'aqua',
+                        'link' => '/admin/orders',
+                        'info' => Order::where('status', 4)->inLastMonth('confirmed_at')->count(),
+                    ],
+                ]
+            ],
+        ];
 
-            $content->row(view('admin.dashboard.title'));
+        foreach ($list as $item) {
+            $content_c = new Content();
+            $content_c->row(function (Row $row) use ($item) {
+                foreach ($item['data'] as $box) {
+                    $row->column($item['width'], function (Column $column) use ($box) {
+                        $column->append(new InfoBox($box['title'], $box['icon'], $box['color'], $box['link'], $box['info']));
+                    });
+                }
+            });
+            $content->row(new Box($item['title'], $content_c->build()));
+        }
 
-            $list = [
-                [
-                    'title' => '今日任务',
-                    'width' => 4,
-                    'data' => [
-                        [
-                            'title' => '待发货',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders?status[]=2',
-                            'info' => Order::where('status', 2)->count(),
-                        ],
-                        [
-                            'title' => '售后待审核',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/refundOrders?status[]=1',
-                            'info' => RefundOrder::where('status', 1)->count(),
-                        ],
-                        [
-                            'title' => '售后待确认',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/refundOrders?status[]=3',
-                            'info' => RefundOrder::where('status', 3)->count(),
-                        ],
-                    ]
-                ],
-                [
-                    'title' => '商场订单下单量',
-                    'width' => 4,
-                    'data' => [
-                        [
-                            'title' => '今日',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inToday('created_at')->count(),
-                        ],
-                        [
-                            'title' => '本周',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inWeek('created_at')->count(),
-                        ],
-                        [
-                            'title' => '本月',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inMonth('created_at')->count(),
-                        ],
-                        [
-                            'title' => '昨日',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inYesterday('created_at')->count(),
-                        ],
-                        [
-                            'title' => '上周',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inLastWeek('created_at')->count(),
-                        ],
-                        [
-                            'title' => '上月',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inLastMonth('created_at')->count(),
-                        ],
-                    ]
-                ],
-                [
-                    'title' => '商场订单支付量',
-                    'width' => 4,
-                    'data' => [
-                        [
-                            'title' => '今日',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inToday('payed_at')->count(),
-                        ],
-                        [
-                            'title' => '本周',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inWeek('payed_at')->count(),
-                        ],
-                        [
-                            'title' => '本月',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inMonth('payed_at')->count(),
-                        ],
-                        [
-                            'title' => '昨日',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inYesterday('payed_at')->count(),
-                        ],
-                        [
-                            'title' => '上周',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inLastWeek('payed_at')->count(),
-                        ],
-                        [
-                            'title' => '上月',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::inLastMonth('payed_at')->count(),
-                        ],
-                    ]
-                ],
-                [
-                    'title' => '商场订单完成量',
-                    'width' => 4,
-                    'data' => [
-                        [
-                            'title' => '今日',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::where('status', 4)->inToday('confirmed_at')->count(),
-                        ],
-                        [
-                            'title' => '本周',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::where('status', 4)->inWeek('confirmed_at')->count(),
-                        ],
-                        [
-                            'title' => '本月',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::where('status', 4)->inMonth('confirmed_at')->count(),
-                        ],
-                        [
-                            'title' => '昨日',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::where('status', 4)->inYesterday('confirmed_at')->count(),
-                        ],
-                        [
-                            'title' => '上周',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::where('status', 4)->inLastWeek('confirmed_at')->count(),
-                        ],
-                        [
-                            'title' => '上月',
-                            'icon' => 'cart-plus',
-                            'color' => 'aqua',
-                            'link' => '/admin/orders',
-                            'info' => Order::where('status', 4)->inLastMonth('confirmed_at')->count(),
-                        ],
-                    ]
-                ],
-            ];
-
-            foreach ($list as $item) {
-                $content_c = new Content();
-                $content_c->row(function (Row $row) use ($item) {
-                    foreach ($item['data'] as $box) {
-                        $row->column($item['width'], function (Column $column) use ($box) {
-                            $column->append(new InfoBox($box['title'], $box['icon'], $box['color'], $box['link'], $box['info']));
-                        });
-                    }
-                });
-                $content->row(new Box($item['title'], $content_c->build()));
-            }
-
-        });
+        return $content;
     }
 }
