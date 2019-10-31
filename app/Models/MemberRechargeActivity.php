1aea472fe0b74147798d702c1b07de25e5bf0bbc
klinson <klinson@163.com>
2019-10-31 18:25:34 +0800

    会员相关表

---
+++ app/Models/MemberRechargeActivity.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MemberRechargeActivity extends Model
+{
+    use SoftDeletes;
+}
---
+++ app/Models/MemberRechargeOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MemberRechargeOrder extends Model
+{
+    use SoftDeletes;
+
+}
