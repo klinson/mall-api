46cccc54e67a84e7df890d3ae94b599f908a3a8a
klinson <klinson@163.com>
2021-02-27 02:08:53 +0800

    收货地址记录详细城市名称

--- database/seeds/AddressesTableSeeder.php
+++ database/seeds/AddressesTableSeeder.php
@@ -27,10 +27,11 @@ class AddressesTableSeeder extends Seeder
                 'city_code' => 440100,
                 'district_code' => 440103,
                 'address' => '广州荔湾区1111',
-                'is_default' => 1,
+                'is_default' => 0,
                 'created_at' => '2019-10-08 22:28:20',
-                'updated_at' => '2019-10-08 22:28:20',
+                'updated_at' => '2021-02-27 02:02:56',
                 'deleted_at' => NULL,
+                'city_name' => '广东省/广州市/荔湾区',
             ),
         ));
         
