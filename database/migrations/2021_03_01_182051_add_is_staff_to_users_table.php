4bb4ee166be8584ffebb245adb8f883077e8174b
klinson <klinson@163.com>
2021-03-02 02:07:48 +0800

    用户管理 职员身份管理

--- database/migrations/2021_03_01_182051_add_is_staff_to_users_table.php
+++ database/migrations/2021_03_01_182051_add_is_staff_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsStaffToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedTinyInteger('is_staff')->nullable();
+            $table->unsignedTinyInteger('is_staff')->default(0);
         });
     }
 
